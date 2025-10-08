4b1e3d173992bc16664143b868b01f4b543ae8be
Abhiram Dadi <abhiramdadi2005@gmail.com>
2025-10-08 17:51:31 +0000

    boom boom

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,41 @@
 import type {NextConfig} from 'next';
 
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?:\/\/.*\/api\/.*/,
+      handler: 'NetworkOnly',
+    },
+    {
+      urlPattern: ({ url }) => {
+        return url.pathname.startsWith('/_next/static');
+      },
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'next-static',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+      },
+    },
+    {
+      handler: 'NetworkFirst',
+      urlPattern: ({ request }) => request.mode === 'navigate',
+      options: {
+        cacheName: 'pages-cache',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
+});
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -32,4 +68,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
